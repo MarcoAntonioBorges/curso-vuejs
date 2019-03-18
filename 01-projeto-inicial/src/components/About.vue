ac42942bc1b263e051a4bf4003e48f97bc972f86
Marco Antonio <mabo.marcoantonio@gmail.com>
2019-03-17 23:57:09 -0300

    Atualizando o projeto
    fazendo a criação das rotas dos componentes

---
+++ 01-projeto-inicial/src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <h1>About us</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+};
+</script>
+
+<style scoped>
+  h1{
+    text-align: center;
+    color: slateblue;
+  }
+</style>
---
+++ 01-projeto-inicial/src/components/Contact.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Contact',
+  data() {
+    return {
+      msg: 'Rua de brasil, 1999',
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>

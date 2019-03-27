0e78f90f8d03b9b2f48d588acd995d676e70acb8
Marco Antonio <mabo.marcoantonio@gmail.com>
2019-03-27 09:28:58 -0300

    Update

--- 02-projeto-vue-material-design/src/App.vue
+++ 02-projeto-vue-material-design/src/App.vue
@@ -10,24 +10,39 @@
         </div>
       </md-card-header>
       <md-card-content>
-        <md-button class="md-raised md-primary" @click="table()">Table</md-button>
-        <md-button class="md-raised md-accent" @click="limpar()">Limpar</md-button>
+        <md-button class="md-raised md-primary" @click="table()">Fill Table</md-button>
+        <md-button class="md-raised md-accent" @click="limpar()">Clear Table</md-button>
       </md-card-content>
 
       <md-table>
-        <md-table-header>
-          <md-table-row>
-            <md-table-head>
-              Primeiro nome
-            </md-table-head>
-            <md-table-head>
-              Sobrenome
-            </md-table-head>
-            <md-table-head>
-              E-mail
-            </md-table-head>
-          </md-table-row>
-        </md-table-header>
+        <md-table-row>
+          <md-table-head>
+            ID
+          </md-table-head>
+          <md-table-head>
+            Primeiro nome
+          </md-table-head>
+          <md-table-head>
+            Sobrenome
+          </md-table-head>
+          <md-table-head>
+            E-mail
+          </md-table-head>
+        </md-table-row>
+        <md-table-row v-for="contato in contatos" :key="contato.id">
+          <md-table-cell md-numeric>
+            {{ contato.id }}
+          </md-table-cell>
+          <md-table-cell>
+            {{ contato.nome }}
+          </md-table-cell>
+          <md-table-cell>
+            {{ contato.sobrenome }}
+          </md-table-cell>
+          <md-table-cell>
+            {{ contato.email }}
+          </md-table-cell>
+        </md-table-row>
       </md-table>
     </md-card>
   </div>
@@ -36,6 +51,27 @@
 <script>
 export default {
   name: 'App',
+  // eslint-disable-next-line
+  data: function () {
+    return {
+      contatos: [
+        { id: 1, nome: 'Marco Antonio', sobrenome: 'Oliveira', email: 'mabo.marcoantonio@gmail.com' },
+        { id: 2, nome: 'Pedro', sobrenome: 'Antonio', email: 'pedro@gmail.com' },
+      ],
+    };
+  },
+  methods: {
+    // eslint-disable-next-line
+    table: function () {
+      this.contatos.push({ nome: 'Flavia menegeri', sobrenome: 'Silva', email: 'flavia@gmail.com' });
+      this.contatos.push({ nome: 'Matheus', sobrenome: 'damassa', email: 'mdamassa@gmail.com' });
+      this.contatos.push({ nome: 'Joao', sobrenome: 'Freitas', email: 'jfreitas@gmail.com' });
+    },
+    // eslint-disable-next-line
+    limpar: function () {
+      this.contatos.splice(0, this.contatos.length);
+    },
+  },
 };
 </script>
 
